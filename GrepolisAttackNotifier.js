5e52b33456e31f65a242fd9bac3e4dbcbf6e20a6
David Knaack <david@knaack.me>
2015-06-08 21:23:36 +0200

    v1.1.1

--- GrepolisAttackNotifier.js
+++ GrepolisAttackNotifier.js
@@ -2,17 +2,19 @@
 // @name          Grepolis Attack Notifier
 // @namespace     github.com/davidkna/Grepolis-Attack-Notifier
 // @description   Get native Browser Notifications when you are attacked on Grepolis! Be sure to attack yourself and grant Grepolis permission to send you notfications 'always'.
-// @version       1.1.0
+// @version       1.1.1
 // @include       *://*.grepolis.*/game*
 // @grant         none
-// @downloadURL   https://raw.githubusercontent.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.user.js
-// @updateURL     https://raw.githubusercontent.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.meta.js
+// @downloadURL   https://cdn.rawgit.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.user.js
+// @updateURL     https://cdn.rawgit.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.meta.js
 // ==/UserScript==
 
 function main() {
 	let lang = 'en';
-	if (Game.market_id in ['de', 'en']) {
-		lang = Game.market_id;
+	const langs = ['de', 'en'];
+	const index = ['de', 'en'].indexOf();
+	if (index >= 0) {
+		lang = langs['index'];
 	}
 
 	$.Observer(GameEvents.attack.incoming)
--- GrepolisAttackNotifier.user.js
+++ GrepolisAttackNotifier.user.js
@@ -2,10 +2,10 @@
 // @name          Grepolis Attack Notifier
 // @namespace     github.com/davidkna/Grepolis-Attack-Notifier
 // @description   Get native Browser Notifications when you are attacked on Grepolis! Be sure to attack yourself and grant Grepolis permission to send you notfications 'always'.
-// @version       1.1.0
+// @version       1.1.1
 // @include       *://*.grepolis.*/game*
 // @grant         none
-// @downloadURL   https://raw.githubusercontent.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.user.js
-// @updateURL     https://raw.githubusercontent.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.meta.js
+// @downloadURL   https://cdn.rawgit.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.user.js
+// @updateURL     https://cdn.rawgit.com/davidkna/Grepolis-Attack-Notifier/master/GrepolisAttackNotifier.meta.js
 // ==/UserScript==
-"use strict";function main(){function i(i){switch(o){case"de":return"Du wirst auf Grepolis angegriffen!\n"+i+"  Angriffe auf dem Weg.";case"en":default:return"You are being attacked on Grepolis!\n"+i+" attacks incoming."}}function e(e){{var t={lang:o,body:i(e)};new Notification("Grepolis Attack Notifier",t)}}function t(e){alert(i(e))}function n(i){"granted"===Notification.permission?e(i):"denied"!==Notification.permission?(alert("Please give Grepolis Attack Notifier permission to send notifications"),Notification.requestPermission(function(n){"permission"in Notification||(Notification.permission=n),"granted"===n?e(i):t(i)})):"denied"===Notification.permission&&t(i)}var o="en";Game.market_id in["de","en"]&&(o=Game.market_id),$.Observer(GameEvents.attack.incoming).subscribe(function(i,e){n(e.count)})}var script=document.createElement("script");script.type="text/javascript",script.innerHTML=""+main.toString()+"main()",document.querySelector("head").appendChild(script);
+"use strict";function main(){function i(i){switch(o){case"de":return"Du wirst auf Grepolis angegriffen!\n"+i+"  Angriffe auf dem Weg.";case"en":default:return"You are being attacked on Grepolis!\n"+i+" attacks incoming."}}function e(e){{var n={lang:o,body:i(e)};new Notification("Grepolis Attack Notifier",n)}}function n(e){alert(i(e))}function t(i){"granted"===Notification.permission?e(i):"denied"!==Notification.permission?(alert("Please give Grepolis Attack Notifier permission to send notifications"),Notification.requestPermission(function(t){"permission"in Notification||(Notification.permission=t),"granted"===t?e(i):n(i)})):"denied"===Notification.permission&&n(i)}var o="en",r=["de","en"],a=["de","en"].indexOf();a>=0&&(o=r.index),$.Observer(GameEvents.attack.incoming).subscribe(function(i,e){t(e.count)})}var script=document.createElement("script");script.type="text/javascript",script.innerHTML=""+main.toString()+"main()",document.querySelector("head").appendChild(script);
